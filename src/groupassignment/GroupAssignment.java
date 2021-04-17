b6d3b4e0e769e06165c6ab882ae7467035dab0ef
ywkok <ywkok16@gmail.com>
2021-04-17 23:26:23 +0800

    for loop

--- src/groupassignment/GroupAssignment.java
+++ src/groupassignment/GroupAssignment.java
@@ -16,6 +16,31 @@ public class GroupAssignment {
      */
     public static void main(String[] args) {
         System.out.println("hello");
+        for(int x=0;x<5;x++)
+        {
+            System.out.println("yeorc");
+        }
+    }
+    
+}
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package groupassignment;
+
+/**
+ *
+ * @author A15
+ */
+public class GroupAssignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello");
         System.out.println("yongwei");
         System.out.println("rongcheng");
     }
