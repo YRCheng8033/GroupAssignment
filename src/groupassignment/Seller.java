eaa0d297c67aa77d490f06f81cb2a6816c8160d1
YRCheng <yrcheng8033@gmail.com>
2021-04-18 23:05:28 +0800

    New update 11.05pm

--- src/groupassignment/Seller.java
+++ src/groupassignment/Seller.java
@@ -14,8 +14,8 @@ public class Seller extends javax.swing.JFrame {
     public Seller() {
         initComponents();
         
-        jTable1.getTableHeader().setOpaque(false);
-        jTable1.getTableHeader().setBackground(new Color(50, 151, 95));
+        ProductTable.getTableHeader().setOpaque(false);
+        ProductTable.getTableHeader().setBackground(new Color(50, 151, 95));
         
     }
 
@@ -44,10 +44,10 @@ public class Seller extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         jList1 = new javax.swing.JList<>();
         jScrollPane2 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        ProductTable = new javax.swing.JTable();
         jPanel4 = new javax.swing.JPanel();
         jLabel7 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        CheckoutButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(255, 255, 255));
@@ -74,24 +74,24 @@ public class Seller extends javax.swing.JFrame {
                         .addGap(92, 92, 92)
                         .addComponent(jLabel2))
                     .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addGap(30, 30, 30)
+                        .addContainerGap()
                         .addComponent(jLabel1)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+            .addGroup(jPanel2Layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel1)
-                .addContainerGap())
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         jLabel3.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel3.setText("Categories Filter");
 
-        CategoriesFilter.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        CategoriesFilter.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "All", "NBA", "NFL", "Pokemon" }));
         CategoriesFilter.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 CategoriesFilterActionPerformed(evt);
@@ -102,7 +102,7 @@ public class Seller extends javax.swing.JFrame {
         jLabel4.setText("Quantity");
 
         jLabel5.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        jLabel5.setText("Cart List");
+        jLabel5.setText("CART LIST");
 
         jLabel6.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jLabel6.setText("Product ID");
@@ -124,7 +124,7 @@ public class Seller extends javax.swing.JFrame {
             public String getElementAt(int i) { return strings[i]; }
         });
         jList1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        jList1.setSelectionForeground(new java.awt.Color(204, 204, 204));
+        jList1.setSelectionBackground(new java.awt.Color(0, 153, 51));
         jScrollPane1.setViewportView(jList1);
 
         javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
@@ -140,7 +140,7 @@ public class Seller extends javax.swing.JFrame {
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        ProductTable.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
                 {null, null, null, null},
@@ -151,24 +151,24 @@ public class Seller extends javax.swing.JFrame {
                 "Product ID", "Categories", "Set Name", "Price"
             }
         ));
-        jTable1.setFocusable(false);
-        jTable1.setIntercellSpacing(new java.awt.Dimension(0, 0));
-        jTable1.setRowHeight(25);
-        jTable1.setSelectionBackground(new java.awt.Color(0, 153, 0));
-        jTable1.setShowVerticalLines(false);
-        jScrollPane2.setViewportView(jTable1);
+        ProductTable.setFocusable(false);
+        ProductTable.setIntercellSpacing(new java.awt.Dimension(0, 0));
+        ProductTable.setRowHeight(25);
+        ProductTable.setSelectionBackground(new java.awt.Color(0, 153, 0));
+        ProductTable.setShowVerticalLines(false);
+        jScrollPane2.setViewportView(ProductTable);
 
         jPanel4.setBackground(new java.awt.Color(204, 204, 204));
 
         jLabel7.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel7.setText("SUB TOTAL :");
 
-        jButton1.setBackground(new java.awt.Color(50, 151, 95));
-        jButton1.setFont(new java.awt.Font("Tahoma", 1, 13)); // NOI18N
-        jButton1.setText("Check Out");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        CheckoutButton.setBackground(new java.awt.Color(50, 151, 95));
+        CheckoutButton.setFont(new java.awt.Font("Tahoma", 1, 13)); // NOI18N
+        CheckoutButton.setText("Check Out");
+        CheckoutButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                CheckoutButtonActionPerformed(evt);
             }
         });
 
@@ -180,7 +180,7 @@ public class Seller extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 126, Short.MAX_VALUE)
-                .addComponent(jButton1)
+                .addComponent(CheckoutButton)
                 .addContainerGap())
         );
         jPanel4Layout.setVerticalGroup(
@@ -189,7 +189,7 @@ public class Seller extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel7)
-                    .addComponent(jButton1))
+                    .addComponent(CheckoutButton))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
@@ -229,7 +229,7 @@ public class Seller extends javax.swing.JFrame {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGap(18, 18, 18)
                 .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -252,7 +252,7 @@ public class Seller extends javax.swing.JFrame {
                         .addGap(43, 43, 43)
                         .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 370, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(102, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -277,11 +277,11 @@ public class Seller extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_SellerProductIDActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        
-        
-        
-    }//GEN-LAST:event_jButton1ActionPerformed
+    private void CheckoutButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_CheckoutButtonActionPerformed
+        new CheckOut();
+        Sellmain.seller.setVisible(false);
+        Sellmain.checkout.setVisible(true);
+    }//GEN-LAST:event_CheckoutButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -315,15 +315,17 @@ public class Seller extends javax.swing.JFrame {
             public void run() {
                 new Seller().setVisible(true);
             }
+            
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton AddCartButton;
     private javax.swing.JComboBox<String> CategoriesFilter;
+    private javax.swing.JButton CheckoutButton;
+    private javax.swing.JTable ProductTable;
     private javax.swing.JTextField SellerProductID;
     private javax.swing.JTextField SellerQuantity;
-    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -338,6 +340,5 @@ public class Seller extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel4;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
 }
---
+++ src/groupassignment/Sellmain.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package groupassignment;
+
+/**
+ *
+ * @author A15
+ */
+public class Sellmain {
+    
+    static Seller seller = new Seller();
+    static CheckOut checkout = new CheckOut();
+    
+}
